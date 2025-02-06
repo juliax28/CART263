9baebbc91b769b08157a4ba5ec9039de08b0b2e8
juliax28 <juliax28@outlook.com>
2025-02-06 14:48:45 -0500

    finished timed events in class

--- InClass/timer_events/js/6_timer.js
+++ InClass/timer_events/js/6_timer.js
@@ -1,4 +1,14 @@
 window.onload = function () {
+  let shades = [
+    "#7fb3d5", //grey blue first
+    "#76d7c4",
+    "#f7dc6f",
+    "#eb984e",
+    "#cb4335",
+    "#8e44ad",
+    "#2e4053",
+    "#e5e7e9",
+  ];
   let gridArray = [];
   for (let i = 0; i < 24; i++) {
     //for each x - make a column of changing y's
@@ -18,4 +28,49 @@ window.onload = function () {
     gridArray[i] = gridCol;
   }
   console.log(gridArray);
+  //the divisor
+let num=2;
+ 
+// for (let i = 0; i < 24; i++) {
+//   for (let j = 0; j < 24; j++) {
+//     //check the j--> y value for color choice (all same ys will have the same color (a row))
+//     let d = gridArray[i][j];
+//     if (j % num === 0) {
+//       d.style.background = shades[0];
+//     } else if (j % num === 1) {
+//       d.style.background = shades[1];
+//     }
+//   }
+// }
+setInterval(animate_cells_mod_rows,1000);
+ 
+function animate_cells_mod_rows() {
+   num+=1;
+   console.log(num);
+  if(num ===9){num=1}
+  for (let i = 0; i < 24; i++) {
+    for (let j = 0; j < 24; j++) {
+        let d = gridArray[i][j]
+  //check the j--> y value for color choice (all same ys will have the same color (a row))
+        if (j % num === 0) {
+          d.style.background = shades[0];
+        } else if (j % num === 1) {
+          d.style.background = shades[1];
+        } else if (j % num === 2) {
+          d.style.background = shades[2];
+        } else if (j % num === 3) {
+          d.style.background = shades[3];
+        } else if (j % num === 4) {
+          d.style.background = shades[4];
+        } else if (j % num === 5) {
+          d.style.background = shades[5];
+        } else if (j % num === 6) {
+          d.style.background = shades[6];
+        } else if (j % num === 7) {
+          d.style.background = shades[7];
+        }
+      }
+    }
 }
+
+};
