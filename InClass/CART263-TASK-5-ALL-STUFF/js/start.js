46c964c21db64e4899bfd91bcbbebb70ada9c91d
juliax28 <juliax28@outlook.com>
2025-03-16 16:13:46 -0400

    tryng to get five circles to appear on cavnas A at random places, not working

--- InClass/CART263-TASK-5-ALL-STUFF/js/start.js
+++ InClass/CART263-TASK-5-ALL-STUFF/js/start.js
@@ -83,14 +83,14 @@ function go_all_stuff() {
 
     let drawingBoardA = new DrawingBoard(theCanvases[0], theContexts[0], theCanvases[0].id);
     //add a circular object to canvas A
-    let numbCircles = [0, 1, 2, 3, 4];
+    let numbCircles = [1, 2, 3, 4, 5];
     for (let i = 0; i < numbCircles.length; i++) {
         let x = Math.random() * 100;
         let y = Math.random() * 100;
         let radius = Math.random() * 100;
         drawingBoardA.addObj(new CircularObj(x, y, radius, "#FFC300", "#E6E6FA", drawingBoardA.context))
         drawingBoardA.display();
-        console.log(numbCircles);
+        console.log(numbCircles.length);
     };
 
 
