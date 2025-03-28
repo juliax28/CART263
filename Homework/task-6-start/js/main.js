f7bf062ac7569ab7dbf4fef5a13ea28687f3728c
juliax28 <juliax28@outlook.com>
2025-03-27 23:36:01 -0400

    reset imeplemented

--- Homework/task-6-start/js/main.js
+++ Homework/task-6-start/js/main.js
@@ -55,14 +55,14 @@ function run() {
       // console.log(seed_phrase_array);
       for (let j = 0; j < nextWord.length; j++) {
         let nextChar = nextWord[j];
-        
+
         for (let k = 0; k < rainbow_tokens.length; k++) {
           let outputWord = rainbow_tokens[k];
 
 
           if (outputWord[j] === nextChar) {
             poem_sentence += outputWord + " ";
-            
+
             break;
           }
 
@@ -80,8 +80,28 @@ function run() {
 
   /****** PART D:: VISUALIZE  */
   function runPartD(new_sentence) {
+
     let outputDiv = document.querySelector("#output");
     outputDiv.style.display = "block";
+    outputDiv.addEventListener("click", animationHandler);
+    let isPlaying = false;
+
+    // example takne form task 03, handles start and stop of animation
+    function animationHandler() {
+      if (isPlaying === false) {
+        isPlaying = true;
+
+        //START
+        lettersAnim = window.requestAnimationFrame(animate);
+
+
+      } else {
+        //STOP
+        window.cancelAnimationFrame(lettersAnim);
+        isPlaying = false;
+
+      }
+    }
     //result is poem_sentence, find each element in the array and then find charAt I guess?
     outputDiv.innerHTML = new_sentence;
     //letter array
@@ -94,40 +114,45 @@ function run() {
       letterDiv.classList.add("letterPos");
 
       letterDiv.innerHTML = letter;
-      letterDiv.style.color = '#' +(Math.random()*0xFFFFFF << 0).toString(16).padStart(6, '0');
+      letterDiv.style.color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
       letterDiv.style.fontSize = Math.random() * 3 + 'em';
       letterDiv.style.fontFamily = "Impact,Charcoal,sans-serif";
       // console.log(letterDiv.style.fontSize);
-      letterDiv.style.left = `${j * 1.5}px`;
+      letterDiv.style.left = `${j * 5}px`;
+      letterDiv.style.top = `${j * 1}px`;
 
-    //   requestAnimationFrame(mousemove);
-    //    addEventListener("mousemove", function (e) {
-    //     mouseOffsetX = parseInt(e.clientX);
-    //     mouseOffsetY = parseInt(e.clientY);
 
-    // });
-
-      
       // letterDiv.style.top = `${j * 40}px`;
       // outputDiv.appendChild(letterDiv);
       letterArray.push(letterDiv);
-      
-           
-  }
-// deleting the original new sentence string
-outputDiv.innerHTML = "";
 
-  for (let i = 0; i < letterArray.length; i ++){
-    outputDiv.appendChild(letterArray[i]);
-    // console.log(letterArray[i]);
+
+    }
+    // deleting the original new sentence string
+    outputDiv.innerHTML = "";
+
+    for (let i = 0; i < letterArray.length; i++) {
+      outputDiv.appendChild(letterArray[i]);
+
+      //the animation
+      function animate() {
+        let speedX = 5;
+        let speedY = 3;
+        letterArray[i].style.left += parseInt(letterArray[i].style.left) + speedX + "px";
+        letterArray[i].style.top += parseInt(letterArray[i].style.top) + speedY + "px";
+
       }
+    }
 
 
-}
+  }
 
   /****** PART E:: RESET  */
   function resetPoem() {
     /*** TO FILL IN */
+    document.querySelector("#output").innerHTML = "";
+    document.querySelector("#output").style.display = "none";
+    document.querySelector("#phrase").value = "";
 
   }
 } //window onload
